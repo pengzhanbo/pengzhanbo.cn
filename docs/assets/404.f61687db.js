c9498493dbeadcd0797abb52f940a9eb39e1dfde
pengzhanbo <q942450674@outlook.com>
2022-03-14 19:35:36 +0800

    chore: delete docs

--- docs/assets/404.f61687db.js
+++
@@ -1,1 +0,0 @@
-import{d as e,u as o,a as t,o as a}from"./app.87b1cd2a.js";const l=e({setup(n){return console.log(o()),(s,c)=>(a(),t("div",null,"404"))}});export{l as default};
--- docs/assets/index.html.89dd264f.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-c9afc8f6",path:"/archives/",title:"\u5F52\u6863",lang:"zh",frontmatter:{title:"\u5F52\u6863",pageType:"archives"},excerpt:"",headers:[]};export{e as data};
