10c39610f43a541aa8fc6dfa1f9f944b11c486f5
pengzhanbo <volodymyr@foxmail.com>
2022-04-06 21:10:10 +0800

    update: theme for support comment

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -47,6 +47,33 @@ export default defineUserConfig<PlumeThemeOptions>({
           },
         },
       },
+      comment: {
+        type: 'giscus',
+        comment: true,
+        repo: 'pengzhanbo/pengzhanbo-blog-vuepress',
+        repoId: 'MDEwOlJlcG9zaXRvcnkxNDgwMzY4MDc=',
+        category: 'Announcements',
+        categoryId: 'DIC_kwDOCNLcx84COcVd',
+        mapping: 'pathname',
+        reactionsEnabled: true,
+        inputPosition: 'top',
+      },
     },
   },
 })
+/**
+<script src="https://giscus.app/client.js"
+        data-repo="pengzhanbo/pengzhanbo-blog-vuepress"
+        data-repo-id="MDEwOlJlcG9zaXRvcnkxNDgwMzY4MDc="
+        data-category="Announcements"
+        data-category-id="DIC_kwDOCNLcx84COcVd"
+        data-mapping="pathname"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="light"
+        data-lang="zh-CN"
+        crossorigin="anonymous"
+        async>
+</script>
+ */
