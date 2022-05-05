30a345534ca1f444cd8be6799e5fc25f70c9c695
pengzhanbo <volodymyr@foxmail.com>
2022-05-05 21:43:08 +0800

    update: 更新文章

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -79,6 +79,55 @@ export default defineUserConfig({
           },
         },
       },
+      // search: false,
+      // docsearch: {
+      //   appId: 'TUDF70KY7U',
+      //   apiKey: '9a9c80617c2cac5406c80f47d87d0031',
+      //   indexName: 'netlify_2d58513b-ff0d-45b2-ad45-21f7eb21eb27_main_all',
+      //   locales: {
+      //     '/zh/': {
+      //       placeholder: '搜索文档',
+      //       translations: {
+      //         button: {
+      //           buttonText: '搜索文档',
+      //           buttonAriaLabel: '搜索文档',
+      //         },
+      //         modal: {
+      //           searchBox: {
+      //             resetButtonTitle: '清除查询条件',
+      //             resetButtonAriaLabel: '清除查询条件',
+      //             cancelButtonText: '取消',
+      //             cancelButtonAriaLabel: '取消',
+      //           },
+      //           startScreen: {
+      //             recentSearchesTitle: '搜索历史',
+      //             noRecentSearchesText: '没有搜索历史',
+      //             saveRecentSearchButtonTitle: '保存至搜索历史',
+      //             removeRecentSearchButtonTitle: '从搜索历史中移除',
+      //             favoriteSearchesTitle: '收藏',
+      //             removeFavoriteSearchButtonTitle: '从收藏中移除',
+      //           },
+      //           errorScreen: {
+      //             titleText: '无法获取结果',
+      //             helpText: '你可能需要检查你的网络连接',
+      //           },
+      //           footer: {
+      //             selectText: '选择',
+      //             navigateText: '切换',
+      //             closeText: '关闭',
+      //             searchByText: '搜索提供者',
+      //           },
+      //           noResultsScreen: {
+      //             noResultsText: '无法找到相关结果',
+      //             suggestedQueryText: '你可以尝试查询',
+      //             reportMissingResultsText: '你认为该查询应该有结果？',
+      //             reportMissingResultsLinkText: '点击反馈',
+      //           },
+      //         },
+      //       },
+      //     },
+      //   },
+      // },
       comment: {
         type: 'giscus',
         comment: true,
