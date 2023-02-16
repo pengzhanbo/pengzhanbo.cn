c78b1c1b24b39c4091441ce9b4849cc09d38f419
pengzhanbo <volodymyr@foxmail.com>
2023-02-17 00:04:36 +0800

    add pwa

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,6 +1,7 @@
 import { path, fs, getDirname } from '@vuepress/utils'
 import { defineUserConfig } from 'vuepress'
 import { googleAnalyticsPlugin  } from '@vuepress/plugin-google-analytics'
+import { pwaPlugin } from '@vuepress/plugin-pwa'
 import theme from './theme'
 
 const __dirname = getDirname(import.meta.url)
@@ -27,9 +28,15 @@ export default defineUserConfig({
   temp: path.resolve(__dirname, '.temp'),
   cache: path.resolve(__dirname, '.cache'),
   head: [
-    ['link', { rel: 'icon', href: '/images/blogger-fav.png' }],
-    ['meta', { 'name': 'keywords', content: '鹏展博,前端，健身' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['meta', { 'name': 'keywords', content: '鹏展博,前端,pengzhanbo,front-end' }],
     ['meta', { 'http-equiv': 'X-UA-Compatible', content: 'id=edg' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
     ['meta', { name: 'msvalidate.01', content: 'F93FF013B8AA2553779A91388C14A0F7'}],
     ['meta', { name: 'google-site-verification', content: 'X5YSaTDn-pKqQBUKD_05_dQcxVItzEq7Rlbg2ZEU7AM' }],
   ],
@@ -38,7 +45,8 @@ export default defineUserConfig({
     googleAnalyticsPlugin({
       id: 'G-TMXNCJR2K7'
     }),
-  ].filter(Boolean) as any,
+    pwaPlugin({ skipWaiting: true, })
+  ],
   onGenerated: (app) => {
     const filepath = app.dir.dest('robots.txt')
     setTimeout(() => {
