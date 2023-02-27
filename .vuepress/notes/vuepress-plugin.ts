c3a76b329b548d89bb07af1e10504979252f6fd1
pengzhanbo <volodymyr@foxmail.com>
2023-02-27 09:34:55 +0800

    update vuepress-plugin

--- .vuepress/notes/vuepress-plugin.ts
+++ .vuepress/notes/vuepress-plugin.ts
@@ -2,21 +2,30 @@ import { definePlumeNotesItemConfig } from '@vuepress-plume/vuepress-theme-plume
 
 export default definePlumeNotesItemConfig({
   dir: 'vuepress-plugin',
-  text: '',
+  text: 'Vuepress Plugin',
   link: '/vuepress-plugin/',
   sidebar: [
-    'caniuse/README',
     {
-      dir: 'netlify-functions',
-      text: 'plugin-netlify-functions',
+      text: 'Vuepress Plugin',
       items: [
-        '',
-        '介绍',
-        '使用',
-        '功能',
-        'API',
-        'functions开发指南',
+        'caniuse/README',
+        'auto-frontmatter/README',
+        'blog-data/README',
+        'notes-data/README',
+        'iconify/README',
+        {
+          dir: 'netlify-functions',
+          text: 'plugin-netlify-functions',
+          link: '/note/vuepress-plugin/netlify-functions/',
+          items: [
+            '介绍',
+            '使用',
+            '功能',
+            'API',
+            'functions开发指南',
+          ]
+        }
       ]
-    }
+    },
   ]
 })
