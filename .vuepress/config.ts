b11427242b15ea3b7e6f46a674e69624f98ff7f7
pengzhanbo <volodymyr@foxmail.com>
2023-12-29 03:51:12 +0800

    update post

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,39 +1,25 @@
-import { fs, getDirname, path } from '@vuepress/utils'
+import { getDirname, path } from '@vuepress/utils'
 import { defineUserConfig } from 'vuepress'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
-
-// import { pwaPlugin } from '@vuepress/plugin-pwa'
 import theme from './theme'
 
 const __dirname = getDirname(import.meta.url)
-
-const robotsContent = `
-# Algolia-Crawler-Verif: A1A1F2E6307A7403
-
-User-agent: *
-Allow: /
-Sitemap: https://pengzhanbo.cn/sitemap.xml
-`
+const resolve = (...dirs: string[]) => path.resolve(__dirname, ...dirs)
 
 export default defineUserConfig({
   lang: 'zh-CN',
   locales: {
-    '/': {
-      lang: 'zh-CN',
-      title: '鹏展博',
-      description: '热爱生活',
-    },
+    '/': { lang: 'zh-CN', title: '鹏展博', description: '热爱生活' },
   },
   dest: 'docs',
-  public: path.resolve(__dirname, '../public'),
-  temp: path.resolve(__dirname, '.temp'),
-  cache: path.resolve(__dirname, '.cache'),
+  public: resolve('../public'),
+  temp: resolve('.temp'),
+  cache: resolve('.cache'),
   head: [
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
     ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }],
-    // ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
     ['meta', { name: 'keywords', content: '鹏展博,前端,pengzhanbo,front-end' }],
     ['meta', { 'http-equiv': 'X-UA-Compatible', 'content': 'IE=edg' }],
     ['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
@@ -43,13 +29,6 @@ export default defineUserConfig({
   ],
   plugins: [
     googleAnalyticsPlugin({ id: 'G-TMXNCJR2K7' }),
-    // pwaPlugin({ skipWaiting: true, })
   ],
-  onGenerated: (app) => {
-    const filepath = app.dir.dest('robots.txt')
-    setTimeout(() => {
-      fs.writeFileSync(filepath, robotsContent, 'utf-8')
-    }, 500)
-  },
   theme,
 })
