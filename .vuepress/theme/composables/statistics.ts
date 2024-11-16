a8ea1f7dae8e19d42245f0c481d73688a82f8c58
pengzhanbo <volodymyr@foxmail.com>
2024-11-17 04:11:51 +0800

    docs: update theme

---
+++ .vuepress/theme/composables/statistics.ts
@@ -0,0 +1,18 @@
+import { watch } from 'vue'
+import { useRoute } from 'vuepress/client'
+
+export function useStatistics() {
+  if (__VUEPRESS_DEV__ || __VUEPRESS_SSR__) return
+  
+  const route = useRoute()
+
+  watch(() => route.path, () => {
+    let img: HTMLImageElement | null = new Image()
+    const { hostname, pathname, protocol } = window.location
+    img.src = 'https://api.pengzhanbo.cn/statistics/collect?url=' + encodeURIComponent(protocol + '//' + hostname + pathname)
+    img.onload = () => {
+      img?.remove()
+      img = null
+    }
+  }, {immediate: true })
+}
--- vuepress.client.ts
+++ vuepress.client.ts
@@ -2,11 +2,15 @@ import { defineClientConfig } from 'vuepress/client'
 import NpmBadge from 'vuepress-theme-plume/features/NpmBadge.vue'
 import NpmBadgeGroup from 'vuepress-theme-plume/features/NpmBadgeGroup.vue'
 import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
+import { useStatistics } from './.vuepress/theme/composables/statistics'
 
 export default defineClientConfig({
   enhance({ app }) {
     app.component('RepoCard', RepoCard)
     app.component('NpmBadge', NpmBadge)
     app.component('NpmBadgeGroup', NpmBadgeGroup)
+  },
+  setup() {
+    useStatistics()
   },
 })
