c92872ec4b2c67d3b41cb9a0b83bc182ba06e71e
pengzhanbo <volodymyr@foxmail.com>
2025-06-18 00:32:36 +0800

    update

--- .vuepress/plume.config.ts
+++ .vuepress/plume.config.ts
@@ -15,8 +15,10 @@ export default defineThemeConfig({
   },
   social: [
     { icon: 'github', link: 'https://github.com/pengzhanbo' },
+    { icon: { svg: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" d="M0 3.121V15h16V3.121l-8 8z"/><path fill="currentColor" d="M16 1H0l8 8z"/></svg>', name: 'email' }, link: 'mailto:hi@pengzhanbo.cn' },
     { icon: 'discord', link: 'https://discord.gg/ZPV9tQyRuh' },
   ],
+  navbarSocialInclude: ['github', 'email', 'discord'],
 
   editLinkText: '在 GitHub 上编辑此页',
   footer: { copyright: 'Copyright © 2021-present pengzhanbo' },
