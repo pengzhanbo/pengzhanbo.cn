e5fc28a1aa75f49da8a0dc88f9fade0e345e8a07
pengzhanbo <volodymyr@foxmail.com>
2026-09-11 12:02:46 +0800

    update

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ export default config({
     'src/面试题/JavaScript/变量.md',
     'src/blog/1.前端/2.工具/jsr使用指南.md',
     'src/blog/1.前端/6.JavaScript/2.进阶/4.执行上下文栈.md',
+    'src/blog/1.前端/1.基础/正则表达式完全指南.md',
   ],
 }, {
   files: ['**/*.md/**/*.{ts,js,vue,jsx,tsx}'],
